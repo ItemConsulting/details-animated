a9fc8343450452a73e3f03c0a55a22226c9ff06d
Tom Arild Jakobsen <tajakobsen@gmail.com>
2024-06-14 16:05:08 +0200

    Add types to story

--- accordion.stories.ts
+++ accordion.stories.ts
@@ -3,10 +3,15 @@ import DetailsAnimated from "./index";
 
 window.customElements.define("details-animated", DetailsAnimated);
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-export default  {
+type Props = {
+  summary: string;
+  details: string;
+  isOpen: boolean;
+}
+
+export default {
   title: 'Details Animated',
-  render: (args) => `
+  render: (args: Props): string => `
     <details-animated>
       <details ${args.isOpen ? "open" : ""}>
         <summary>${args.summary}</summary>
@@ -14,9 +19,9 @@ export default  {
       </details>
     </details-animated>
     `,
-} satisfies Meta
+} satisfies Meta<Props>
 
-export const detailsAnimated: StoryObj = {
+export const detailsAnimated: StoryObj<Props> = {
   args: {
     summary: "Learn latin here!",
     details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
